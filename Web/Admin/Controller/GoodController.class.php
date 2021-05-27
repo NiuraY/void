9ddad5a1a1de329937c474e59c7a09d696d33435
NiuraY <qkciw40jbdb2771@126.com>
2021-05-27 23:01:16 +0800

    first

---
+++ Web/Admin/Controller/GoodController.class.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * @Author: Vold
+ * @Date:   2017-11-14 19:53:43
+ * @Last Modified by:   Vold
+ * @Last Modified time: 2017-11-14 19:54:21
+ */
+namespace Admin\Controller;
+use Think\
